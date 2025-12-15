6bb79a6dbf6bb096d1a9a58b7eec43c7aec60da4
roriGit <rori.madonko@gmail.com>
2025-12-15 10:44:05 +0200

    Add routes for the admin pages

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::middleware('auth')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
     // Protected routes
-    Route::get('/', [App\Http\Controllers\ProductController::class, 'index'])->name('products');
+    //Route::get('/', [App\Http\Controllers\ProductController::class, 'index'])->name('products');
     Route::post('/cart/add', [App\Http\Controllers\CartController::class, 'add'])->name('cart.add');
     Route::get('/cart', [App\Http\Controllers\CartController::class, 'view'])->name('cart.view');
     Route::post('/cart/remove/{id}', [App\Http\Controllers\CartController::class, 'remove'])->name('cart.remove');
@@ -34,3 +34,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/payfast/success', [App\Http\Controllers\PayFastController::class, 'success'])->name('payment.success');
     Route::get('/payfast/cancel', [App\Http\Controllers\PayFastController::class, 'cancel'])->name('payment.cancel');
 });
+
+//these will need to be added for admin
+    Route::get('/phones', [UCaseDesignController::class, 'list'])->name('phones.index');
+    Route::post('/phones', [UCaseDesignController::class, 'store'])->name('phones.store');
